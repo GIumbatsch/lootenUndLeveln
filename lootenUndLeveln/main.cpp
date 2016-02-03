ef315aa26370a52e970fa03b7608d9b7ef817cd3
GIumbatsch <davidsiepen@outlook.com>
2016-02-03 14:57:46 +0100

    krasser sfml kack

--- lootenUndLeveln/main.cpp
+++ lootenUndLeveln/main.cpp
@@ -1,10 +1,24 @@
-#include <iostream>
-#include <stdio.h>
+#include <SFML/Graphics.hpp>
 
-using namespace std;
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
 
-int main() {
-	// sfml pls :3
-	cout << "test" << endl;
-	getchar();
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
 }
