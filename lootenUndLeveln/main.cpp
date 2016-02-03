b528349e4402884a2b8b8ad620b778b8388deca9
GIumbatsch <davidsiepen@outlook.com>
2016-02-03 17:19:52 +0100

    Krasser Player :3

--- lootenUndLeveln/main.cpp
+++ lootenUndLeveln/main.cpp
@@ -3,26 +3,49 @@
 
 bool moveRight = true;
 
-void moveShape(sf::Shape &s, sf::RenderWindow &window)
+void moveShape(sf::Shape &s, sf::RenderWindow &window, float speed)
 {
 	std::cout << "WindowX = " << window.getSize().x << "| xPos = " << s.getPosition().x << std::endl;
 	if (moveRight && s.getPosition().x > window.getSize().x)
 	{
 		moveRight = false;
 	}
-	else if(!moveRight && s.getPosition().x < 0)
+	else if (!moveRight && s.getPosition().x < 0)
 	{
 		moveRight = true;
 	}
 	if (moveRight)
 	{
-		s.move(1.0f, 0.0f);
+		s.move(1.0f*speed, 0.0f);
 	}
 	else
 	{
-		s.move(-1.0f, 0.0f);
+		s.move(-1.0f*speed, 0.0f);
 	}
 
+}
+
+void movePlayer(sf::Shape &player) {
+
+	sf::Vector2f movement(0.f,0.f);
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		movement.x -= 1.f;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		movement.x += 1.f;
+	}
+	
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		movement.y -= 1.f;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		movement.y += 1.f;
+	}
+
+	player.move(movement);
 }
 
 
@@ -30,14 +53,24 @@ void moveShape(sf::Shape &s, sf::RenderWindow &window)
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1024, 720), "SFML works!");
-	
+
 	sf::CircleShape shape1(100.f);
 	shape1.setFillColor(sf::Color::White);
 
-	sf::Vector2f size(200.f,200.f);
+	sf::Vector2f size(200.f, 200.f);
 	sf::RectangleShape shape2(size);
+	shape2.move(0.f, 200.0f);
 	shape2.setFillColor(sf::Color::Green);
 
+	sf::RectangleShape shape3(size);
+	shape3.move(window.getSize().x - shape3.getSize().x,200.f);
+	shape3.setFillColor(sf::Color::Yellow);
+
+
+	sf::RectangleShape player(size*0.5f);
+	player.move(window.getSize().x / 2.f, window.getSize().y - 200.f);
+	player.setOutlineColor(sf::Color::Blue);
+	player.setOutlineThickness(5.f);
 
 	while (window.isOpen())
 	{
@@ -48,11 +81,16 @@ int main()
 				window.close();
 		}
 
-		moveShape(shape1, window);
+		moveShape(shape1, window, 1.f);
+		moveShape(shape2, window, 0.5f);
+		moveShape(shape3,window, -0.5f);
+		movePlayer(player);
 
 		window.clear();
 		window.draw(shape1);
 		window.draw(shape2);
+		window.draw(shape3);
+		window.draw(player);
 		window.display();
 	}
 
