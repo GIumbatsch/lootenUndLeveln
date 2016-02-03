3b3811fa4d441759c600e372beb94cf076f315bf
GIumbatsch <davidsiepen@outlook.com>
2016-02-03 15:57:52 +0100

    Krasse Kreise und Rechtecke

--- lootenUndLeveln/main.cpp
+++ lootenUndLeveln/main.cpp
@@ -1,10 +1,43 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+bool moveRight = true;
+
+void moveShape(sf::Shape &s, sf::RenderWindow &window)
+{
+	std::cout << "WindowX = " << window.getSize().x << "| xPos = " << s.getPosition().x << std::endl;
+	if (moveRight && s.getPosition().x > window.getSize().x)
+	{
+		moveRight = false;
+	}
+	else if(!moveRight && s.getPosition().x < 0)
+	{
+		moveRight = true;
+	}
+	if (moveRight)
+	{
+		s.move(1.0f, 0.0f);
+	}
+	else
+	{
+		s.move(-1.0f, 0.0f);
+	}
+
+}
+
+
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(1024, 720), "SFML works!");
+	
+	sf::CircleShape shape1(100.f);
+	shape1.setFillColor(sf::Color::White);
+
+	sf::Vector2f size(200.f,200.f);
+	sf::RectangleShape shape2(size);
+	shape2.setFillColor(sf::Color::Green);
+
 
 	while (window.isOpen())
 	{
@@ -15,8 +48,11 @@ int main()
 				window.close();
 		}
 
+		moveShape(shape1, window);
+
 		window.clear();
-		window.draw(shape);
+		window.draw(shape1);
+		window.draw(shape2);
 		window.display();
 	}
 
