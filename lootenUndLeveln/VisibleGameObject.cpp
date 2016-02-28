fea461bddda26bffb593f6e6d1003792cf472e76
Sinan <sinan.sagunc@gmail.com>
2016-02-28 22:35:34 +0100

    VisibleGameObject

---
+++ lootenUndLeveln/VisibleGameObject.cpp
@@ -0,0 +1,49 @@
+#include "stdafx.h"
+#include "VisibleGameObject.h"
+
+VisibleGameObject::VisibleGameObject()
+{
+	_isLoaded == false;
+}
+
+VisibleGameObject::~VisibleGameObject()
+{
+}
+
+//Laden des Objektes
+void VisibleGameObject::load(std::string filename)
+{
+	//Falls keine Textur gefunden wird
+	if (_image.loadFromFile(filename) == false)
+	{
+		_filename = "";
+		//nicht laden lassen
+		_isLoaded = false;
+	}
+	else
+	{
+		//ansonsten alle Member initialisieren
+		_filename = filename;
+		_sprite.setTexture(_image);
+		_isLoaded = true;
+	}
+}
+
+void VisibleGameObject::draw(sf::RenderWindow &window)
+{
+	//Wenn das Bild geladen wurde
+	if (_isLoaded)
+	{
+		//zeichnen
+		window.draw(_sprite);
+	}
+}
+
+//Position des Sprites ändern
+void VisibleGameObject::setPosition(float x, float y)
+{
+	if (_isLoaded)
+	{
+		_sprite.setPosition(x, y);
+	}
+}
---
+++ lootenUndLeveln/VisibleGameObject.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "stdafx.h"
+
+class VisibleGameObject
+{
+public:
+	VisibleGameObject();
+	virtual ~VisibleGameObject();
+
+	virtual void load(std::string filename);
+	virtual void draw(sf::RenderWindow &window);
+
+	virtual void setPosition(float x, float y);
+
+private:
+	sf::Sprite _sprite;
+	sf::Texture _image;
+	std::string _filename;
+	bool _isLoaded;
+};
