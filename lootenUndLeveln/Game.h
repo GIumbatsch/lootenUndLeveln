50b87b0c8bacdf112d76c24e8242565fda17e250
Sinan <sinan.sagunc@gmail.com>
2016-02-29 23:27:20 +0100

    blub

---
+++ lootenUndLeveln/Game.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "SFML\Window.hpp"
+#include "SFML\Graphics.hpp"
+#include "SplashScreen.h"
+#include "MainMenu.h"
+#include "GameObjectManager.h"
+
+class Game
+{
+public:
+	static void Start();
+	static sf::RenderWindow& GetWindow();
+	const static int SCREEN_WIDTH = 800;
+	const static int SCREEN_HEIGHT = 600;
+
+private:
+	static bool isExiting();
+	static void GameLoop();
+
+	static void showSplashScreen();
+	static void showMenu();
+
+	//Die verschiedenen Statuse in denen sich das Spiel befinden kann
+	enum GameState { Uninitialized, ShowingSplash, Paused, ShowingMenu, Playing, Exiting };
+
+	static GameState _gameState;
+	static sf::RenderWindow _mainWindow;
+	
+	static GameObjectManager _gameObjectManager;
+};
