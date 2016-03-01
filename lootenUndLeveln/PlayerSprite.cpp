79c0b11b7f09432d7f8c0e6d6113302a84b9eb7e
Sinan <sinan.sagunc@gmail.com>
2016-03-01 22:22:19 +0100

    nichts wirklich neues

--- lootenUndLeveln/PlayerSprite.cpp
+++ lootenUndLeveln/PlayerSprite.cpp
@@ -2,7 +2,7 @@
 #include "PlayerSprite.h"
 #include "Game.h"
 
-PlayerSprite::PlayerSprite() : _velocity(0.0f), _maxVelocity(.3f), _acceleration(.005f)
+PlayerSprite::PlayerSprite() : _velocity(0.0f), _maxVelocity(.3f), _acceleration(.0005f)
 {
 	load("images/PlayerPrototype.png");
 	assert(isLoaded());
@@ -30,19 +30,17 @@ void PlayerSprite::update(float elapsedTime)
 	//std::cout << "PlayerSprite- Time: " << elapsedTime << std::endl;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
 	{
-		//_velocity -= 30.0f;
 		_velocity = _velocity - 0.5f * _acceleration * (elapsedTime * elapsedTime);
 		std::cout << _velocity << " " << _acceleration << " " << elapsedTime << std::endl;
 	}
-	
+	/*
 	if (!(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)))
 	{
 		_velocity = 0.0f;
 	}
-	
+	*/
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
 	{
-		//	_velocity += 30.0f;
 		_velocity = _velocity + 0.5f * _acceleration * (elapsedTime * elapsedTime);
 		std::cout << _velocity << " " << _acceleration << " " << elapsedTime << std::endl;
 	}
